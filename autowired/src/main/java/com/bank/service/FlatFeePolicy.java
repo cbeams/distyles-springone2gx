8d832aa197f14ea9e5a5706d2c59e04ca68fc2b3
Chris Beams <cbeams@vmware.com>
2009-10-20 19:34:56 +0000

    adding baseline for the rest of the projects

---
+++ autowired/src/main/java/com/bank/service/FlatFeePolicy.java
@@ -0,0 +1,15 @@
+package com.bank.service;
+
+public class FlatFeePolicy implements FeePolicy {
+
+	private final double flatFee;
+
+	public FlatFeePolicy(double flatFee) {
+		this.flatFee = flatFee;
+	}
+	
+	public double calculateFee(double transferAmount) {
+		return flatFee;
+	}
+
+}
