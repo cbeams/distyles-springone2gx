3b39948c573fb75587b3f8f224cfa9384c96fff4
Chris Beams <cbeams@vmware.com>
2009-10-21 01:04:22 +0000

    changes for xml project as developed in the demo

--- xml/src/main/java/com/bank/app/TellerBootstrap.java
+++ xml/src/main/java/com/bank/app/TellerBootstrap.java
@@ -2,23 +2,14 @@ package com.bank.app;
 
 import java.io.IOException;
 
-import com.bank.repository.AccountRepository;
-import com.bank.repository.InMemoryAccountRepository;
-import com.bank.service.FlatFeePolicy;
-import com.bank.service.FeePolicy;
-import com.bank.service.TransferService;
-import com.bank.service.TransferServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TellerBootstrap {
 
 	public static void main(String... args) throws IOException {
-
-		AccountRepository accountRepository = new InMemoryAccountRepository();
-		FeePolicy feePolicy = new FlatFeePolicy(2.00);
-		TransferService transferService = new TransferServiceImpl(accountRepository, feePolicy);
-		transferService.setMinimumTransferAmount(10.00);
-		TellerUI tellerUI = new TellerUI(transferService);
-		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/bank/app/app-config.xml");
+		TellerUI tellerUI = (TellerUI) ctx.getBean("tellerUI");
 		tellerUI.start();
 	}
 
