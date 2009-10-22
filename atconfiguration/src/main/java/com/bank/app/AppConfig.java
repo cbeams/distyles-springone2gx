d14680b3e365b83bcaf67f5da302506aab5251f0
Chris Beams <cbeams@vmware.com>
2009-10-22 15:26:47 +0000

    changes for @Configuration project as developed in the demo

---
+++ atconfiguration/src/main/java/com/bank/app/AppConfig.java
@@ -0,0 +1,40 @@
+package com.bank.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.bank.repository.AccountRepository;
+import com.bank.repository.InMemoryAccountRepository;
+import com.bank.service.FeePolicy;
+import com.bank.service.FlatFeePolicy;
+import com.bank.service.TransferService;
+import com.bank.service.TransferServiceImpl;
+
+@Configuration
+public class AppConfig {
+	
+	@Value("${flatfee.policy") 
+	private double flatFeePolicy;
+	
+	@Bean
+	public TellerUI tellerUI() {
+		return new TellerUI(transferService());
+	}
+	
+	@Bean
+	public TransferService transferService() {
+		return new TransferServiceImpl(accountRepository(), feePolicy());
+	}
+
+
+	@Bean
+	public FeePolicy feePolicy() {
+		return new FlatFeePolicy(flatFeePolicy);
+	}
+
+	@Bean
+	public AccountRepository accountRepository() {
+		return new InMemoryAccountRepository();
+	}
+}
